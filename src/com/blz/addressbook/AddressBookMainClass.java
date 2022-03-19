3f294d6b6e4d441e8ac87267724954f1abeb6d16
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-19 21:39:36 +0530

    Added:ability to create a contact

---
+++ src/com/blz/addressbook/AddressBookMainClass.java
@@ -0,0 +1,10 @@
+package com.blz.addressbook;
+
+public class AddressBookMainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+
+	}
+
+}
